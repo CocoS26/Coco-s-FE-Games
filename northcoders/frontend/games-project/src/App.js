060046fc60334e415ff2272fde7201cbcc9b2353
CocoS26 <Coco.Shen@KPMG.co.uk>
2023-01-09 11:28:40 +0000

    Header

---
+++ northcoders/frontend/games-project/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from './components/Header'
+import Reviews from './components/Reviews'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Reviews />
+    </div>
+  );
+}
+
+export default App;
---
+++ northcoders/frontend/games-project/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header =()=>{
+    return (
+        <>
+        <h1>
+            Find out your favourite games!
+        </h1>
+
+        </>
+        
+    );
+}
+export default Header;
