f996baf971e3b6fdc43938ff828462df18bcebdd
CocoS26 <Coco.Shen@KPMG.co.uk>
2023-01-11 11:45:00 +0000

    add project

---
+++ src/components/CommentAdder.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const CommentAdder = () =>{
+    const [newComment, setNewComment] = useState('');
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        // postComment(newComment)
+        // .then
+    };
+    return (
+        <form className ="CommentAdder" onSubmit={handleSubmit}>
+            <label htmlFor ="newComment">Add a comment</label>
+            <textarea 
+            id= "newComment"
+            value ={newComment}
+            onChange={(e)=>{setNewComment(e.target.value)}}
+            ></textarea>
+            <button>Add</button>
+        </form>
+    )
+}
+
+export default CommentAdder;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import{ getCategories } from "../utils/api";
+
+const Nav = () =>{
+    const [categories, setCategories] = useState([]);
+    
+    useEffect(()=>{
+        getCategories()
+        .then(({categories})=>{
+            setCategories(categories)
+        })
+    },[])
+
+
+return <nav className = "Nav">
+    <ul>
+    {categories.map(category=>{
+        return <Link to={`/reviews?category=${category.slug}`} className="Nav__link" key= {category.slug}><li>{category.slug}</li>
+        </Link>
+    })}
+    <Link to='/reviews' className ="Nav__link">
+        All
+    </Link>
+</ul>
+</nav>
+
+}
+
+export default Nav
